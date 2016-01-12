1bfcd0041241e66f5c77e7dd5b28591218bb8dd8
Tony Huang <tony@huang.ninja>
2016-01-13 07:18:38 +0800

    Remove all semicolon and change to use strict mode.

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,14 @@
-var bcrypt = require('bcryptjs');
-var GitModule = require('../modules/git');
-var UserModule = require('../modules/user');
+'use strict'
+
+var bcrypt = require('bcryptjs')
+var GitModule = require('../modules/git')
+var UserModule = require('../modules/user')
 
 var listRepository = function(req, res, next) {
+	var userData = req.session.userData
+
 	// List all repositories
-	GitModule.listRepo(app.settings.config.gitPath).then(function(result) {
+	GitModule.listRepo(req.app.settings.config.gitPath).then(function(result) {
 		var resp = {
 			code: 200,
 			result: 'OK',
@@ -15,50 +19,53 @@ var listRepository = function(req, res, next) {
 			action: 'List Repository',
 			req: userData,
 			result: resp
-		});
-		res.json(resp);
-		res.end();
-		return;
+		})
+		res.json(resp)
+		res.end()
+		return
 	}, function(err) {
 		req.log.error({
 			catalog: 'Admin',
 			action: 'List Repository',
 			req: userData,
 			error: err
-		});
-		res.status(500).send('Server Error: ' + err);
-		return;
-	});
-};
+		})
+		res.status(500).send('Server Error: ' + err)
+		return
+	})
+}
 
 var listUser = function(req, res, next) {
+	var userData = req.session.userData
+
 	// List all repositories
-	var User = UserModule.init(db, app.settings.config.database.type);
+	var User = UserModule.init(req.db, req.app.settings.config.database.type)
 	User.list().then(function(result) {
 		req.log.info({
 			catalog: 'Admin',
 			action: 'List User',
 			req: userData,
 			result: result
-		});
-		res.json(result);
-		res.end();
-		return;
+		})
+		res.json(result)
+		res.end()
+		return
 	}, function(err) {
 		req.log.error({
 			catalog: 'Admin',
 			action: 'List User',
 			req: userData,
 			error: err
-		});
-		res.status(500).send('Server Error: ' + err);
-		return;
-	});
-};
+		})
+		res.status(500).send('Server Error: ' + err)
+		return
+	})
+}
 
 var addUser = function(req, res, next) {
-	var username = req.body.username;
-	var password = req.body.password;
+	var userData = req.session.userData
+	var username = req.body.username
+	var password = req.body.password
 
 	var userData = {
 		username: username,
@@ -68,20 +75,20 @@ var addUser = function(req, res, next) {
 		collaborateRepositoryList: [],
 		type: UserModule.USER_TYPE_NORMAL
 	}
-	var User = UserModule.init(db, app.settings.config.database.type);
+	var User = UserModule.init(req.db, req.app.settings.config.database.type)
 	User.signup(userData).then(function(result) {
 		if (result.code == UserModule.USER_OK.code) {
-			return User.list();
+			return User.list()
 		} else {
 			req.log.info({
 				catalog: 'Admin',
 				action: 'Add User',
 				req: userData,
 				result: result
-			});
-			res.json(result);
-			res.end();
-			return;
+			})
+			res.json(result)
+			res.end()
+			return
 		}
 	}, function(err) {
 		req.log.error({
@@ -89,9 +96,9 @@ var addUser = function(req, res, next) {
 			action: 'Add User',
 			req: userData,
 			error: err
-		});
-		res.status(500).send('Server Error: ' + err);
-		return;
+		})
+		res.status(500).send('Server Error: ' + err)
+		return
 	})
 
 	// Result of list user
@@ -101,30 +108,31 @@ var addUser = function(req, res, next) {
 			action: 'Add User',
 			req: userData,
 			result: result
-		});
-		res.json(result);
-		res.end();
-		return;
+		})
+		res.json(result)
+		res.end()
+		return
 	}, function(err) {
 		req.log.error({
 			catalog: 'Admin',
 			action: 'Add User',
 			req: userData,
 			error: err
-		});
-		res.status(500).send('Server Error: ' + err);
-		return;
-	});
-};
+		})
+		res.status(500).send('Server Error: ' + err)
+		return
+	})
+}
 
 var deleteUser = function(req, res, next) {
-	var username = req.body.username;
+	var userData = req.session.userData
+	var username = req.body.username
 
 	// List all repositories
-	var User = UserModule.init(db, app.settings.config.database.type);
+	var User = UserModule.init(req.db, req.app.settings.config.database.type)
 	User.deleteUser(username).then(function(result) {
 		if (result.code == UserModule.USER_OK.code) {
-			return User.list();
+			return User.list()
 		} else {
 			req.log.info({
 				catalog: 'Admin',
@@ -134,10 +142,10 @@ var deleteUser = function(req, res, next) {
 					deleteUsername: username
 				},
 				result: result
-			});
-			res.json(result);
-			res.end();
-			return;
+			})
+			res.json(result)
+			res.end()
+			return
 		}
 	}, function(err) {
 		req.log.error({
@@ -148,9 +156,9 @@ var deleteUser = function(req, res, next) {
 				deleteUsername: username
 			},
 			error: err
-		});
-		res.status(500).send('Server Error: ' + err);
-		return;
+		})
+		res.status(500).send('Server Error: ' + err)
+		return
 	})
 
 	// Result of list user
@@ -160,23 +168,25 @@ var deleteUser = function(req, res, next) {
 			action: 'Delete User',
 			req: userData,
 			result: result
-		});
-		res.json(result);
-		res.end();
-		return;
+		})
+		res.json(result)
+		res.end()
+		return
 	}, function(err) {
 		req.log.error({
 			catalog: 'Admin',
 			action: 'Delete User',
 			req: userData,
 			error: err
-		});
-		res.status(500).send('Server Error: ' + err);
-		return;
-	});
-};
+		})
+		res.status(500).send('Server Error: ' + err)
+		return
+	})
+}
 
 var checkAdminPermission = function(req, res, next) {
+	var userData = req.session.userData
+	
 	// Check permission
 	if (userData.type < UserModule.USER_TYPE_ADMIN) {
 		req.log.info({
@@ -184,11 +194,11 @@ var checkAdminPermission = function(req, res, next) {
 			action: 'Check Admin Permission',
 			req: userData,
 			result: 'No Permission.'
-		});
-		res.status(403).send('No Permission');
-		return;
+		})
+		res.status(403).send('No Permission')
+		return
 	}
-	next();
+	next()
 }
 
 

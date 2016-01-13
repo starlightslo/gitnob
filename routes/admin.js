ed2a57634bcd491cf0284b56565191328e6be2fb
Tony Huang <tony@huang.ninja>
2016-01-13 08:22:19 +0800

    Add the data validation.

--- routes/admin.js
+++ routes/admin.js
@@ -67,6 +67,44 @@ var addUser = function(req, res, next) {
 	var username = req.body.username
 	var password = req.body.password
 
+	// Check value of input
+	var err = ''
+	if (!username || !password) {
+		err = 'The input data is empty.'
+	}
+
+	// Check length
+	var minNumOfChars = 6
+	if (err.length == 0 && username.length < minNumOfChars) {
+		err = 'The username must be at least 6 characters.'
+	}
+	if (err.length == 0 && password.length < minNumOfChars) {
+		err = 'The password must be at least 6 characters.'
+	}
+
+	// Check special characters
+	var regularExpression = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/
+	if(err.length == 0 && !regularExpression.test(password)) {
+		err = 'The password should not contain some special character.'
+	}
+
+	// Check error
+	if (err.length > 0) {
+		req.log.error({
+			catalog: 'Admin',
+			action: 'Add User',
+			req: {
+				userData: userData,
+				username: username,
+				password: password
+			},
+			error: err
+		})
+		res.status(400).send(err)
+		res.end()
+		return
+	}
+
 	var userData = {
 		username: username,
 		password: bcrypt.hashSync(password),
@@ -128,6 +166,29 @@ var deleteUser = function(req, res, next) {
 	var userData = req.session.userData
 	var username = req.body.username
 
+	// Check value of input
+	var err = ''
+	if (!username) {
+		err = 'The input data is empty.'
+	}
+
+	// Check error
+	if (err.length > 0) {
+		req.log.error({
+			catalog: 'Admin',
+			action: 'Delete User',
+			req: {
+				userData: userData,
+				username: username,
+				password: password
+			},
+			error: err
+		})
+		res.status(400).send(err)
+		res.end()
+		return
+	}
+
 	// List all repositories
 	var User = UserModule.init(req.db, req.app.settings.config.database.type)
 	User.deleteUser(username).then(function(result) {
@@ -186,7 +247,7 @@ var deleteUser = function(req, res, next) {
 
 var checkAdminPermission = function(req, res, next) {
 	var userData = req.session.userData
-	
+
 	// Check permission
 	if (userData.type < UserModule.USER_TYPE_ADMIN) {
 		req.log.info({
